a698ee041262c180b4026caae1add5bb5891c0c0
Mrgl1203 <gl152217@163.com>
2019-04-21 21:48:17 +0800

    when语句

--- app/src/main/java/com/gulei/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/gulei/hellokotlin/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.TextView
+import com.gulei.hellokotlin.kbean.Color
 import com.gulei.hellokotlin.kbean.Person
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +23,10 @@ class MainActivity : AppCompatActivity() {
         print("最大值打印变量方法为$maxValue")
 
         print("表达式中可以添加双引号字符串打印为${if (max(1,2)>3) "条件成立" else "条件不成立"}")
+
+        print(whenMethod(Color.RED))
+
+        print(whenWithSet(Color.RED,Color.GREEN))
     }
 
     fun print(msg: String) {
@@ -34,6 +39,22 @@ class MainActivity : AppCompatActivity() {
             a
         } else {
             b
+        }
+    }
+
+    fun whenMethod(c:Color):String{
+      return  when(c){
+            Color.RED ->"Red"
+            Color.GREEN ->"Green"
+            else ->"Blue"
+        }
+    }
+    //setOf无关参数顺序，只要在set中就可以，when会先执行之前的代码段后退出
+    fun whenWithSet(c1:Color,c2:Color):String{
+        return  when(setOf(c1,c2)){
+//            setOf(Color.RED,Color.GREEN) ->"Red And Green"
+            setOf(Color.GREEN,Color.RED) ->"Green And Red"
+            else ->"else"
         }
     }
 
--- app/src/main/java/com/gulei/hellokotlin/kbean/Color.kt
+++ app/src/main/java/com/gulei/hellokotlin/kbean/Color.kt
@@ -4,6 +4,6 @@ package com.gulei.hellokotlin.kbean
  * Created by gl152 on 2019/4/11.
  * 枚举
  */
-enum class Color {
-
+enum class Color(val r: Int, val g: Int, val b: Int) {
+    RED(255,0,0), GREEN(0,255,0), BLUE(0,0,255)
 }
